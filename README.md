304e30008b3fbdb417c4f6137b971fd2a6543ae4
Elie <79168733+el2zay@users.noreply.github.com>
2025-05-17 23:37:09 +0200

    Nouveaux ajouts

--- README.md
+++
@@ -1,3 +0,0 @@
-# chiffresetlettres
-
-A new Flutter project.
--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 List lettres = [];
 List boiteLettres = [];
 List usedLettres = [];
-List mottrouves = [];
+List motTrouves = [];
 
 void main() {
   lettres = choisir_lettres();
@@ -24,11 +24,63 @@ class MainApp extends StatefulWidget {
 }
 
 class _MainAppState extends State<MainApp> {
+  List<Widget> buildLettres(length, onTap, type) {
+    return List.generate(
+      length,
+      (index) {
+        return Stack(
+          children: [
+            GestureDetector(
+              onTap: () => onTap(index),
+              child: MouseRegion(
+                cursor: usedLettres.contains(index) && type == 1 ? SystemMouseCursors.forbidden : SystemMouseCursors.click,
+                child: Card(
+                  color: usedLettres.contains(index) && type == 1 ? Colors.grey[350] : Colors.white,
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                  margin: EdgeInsets.symmetric(horizontal: 5),
+                  child: SizedBox(
+                    width: 50,
+                    height: 50,
+                    child: Center(
+                      child: type == 1
+                          ? Text(
+                              lettres[index],
+                              style: TextStyle(
+                                fontSize: 20,
+                                color: Colors.cyan[900],
+                                fontWeight: FontWeight.bold,
+                              ),
+                            )
+                          : Text(
+                              boiteLettres.isEmpty ? "" : boiteLettres[index],
+                              style: TextStyle(
+                                fontSize: 20,
+                                color: Colors.cyan[900],
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            if (type == 1)
+              Positioned(
+                top: 0,
+                right: 0,
+                child: Image.asset("assets/ruban.png", width: 20, height: 20),
+              ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     var height = MediaQuery.of(context).size.height;
     var width = MediaQuery.of(context).size.width;
-    
+
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Color.fromRGBO(248, 143, 175, 1),
@@ -53,9 +105,11 @@ class _MainAppState extends State<MainApp> {
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(10),
                 ),
-                child: SizedBox(
+                child: Container(
                   width: width * 0.6,
                   height: height * 0.5,
+                  margin: EdgeInsets.symmetric(horizontal: 20),
+
                   child: Column(
                     children: [
                       SizedBox(height: 10),
@@ -68,7 +122,31 @@ class _MainAppState extends State<MainApp> {
                         ),
                       ),
                       SizedBox(height: 10),
-                      // TODO
+                      Expanded(
+                        child: GridView.builder(
+                          itemCount: motTrouves.length,
+                          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 6,
+                            childAspectRatio: 2,
+                            crossAxisSpacing: 10,
+                          ),
+                          itemBuilder: (context, index) {
+                            return Card(
+                              color: Color.fromRGBO(242, 200, 214, 1),
+                              child: Center(
+                                child: Text(
+                                  motTrouves[index],
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    color: Colors.cyan[900],
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                      ),
                     ],
                   ),
                 ),
@@ -84,39 +162,15 @@ class _MainAppState extends State<MainApp> {
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      ...List.generate(
+                      ...buildLettres(
                         boiteLettres.length,
                         (index) {
-                          return GestureDetector(
-                            onTap: () {
-                              setState(() {
-                                boiteLettres.removeAt(index);   
-                                usedLettres.removeAt(index);
-                              });
-                            },
-                            child: Card(
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10),
-                              ),
-                              child: SizedBox(
-                                width: 50,
-                                height: 50,
-                                child: Center(
-                                  child: Text(
-                                    boiteLettres.isEmpty
-                                        ? ""
-                                        : boiteLettres[index].toString(),
-                                    style: TextStyle(
-                                      fontSize: 20,
-                                      color: Colors.cyan[900],
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ),
-                          );
+                          setState(() {
+                            boiteLettres.removeAt(index);
+                            usedLettres.removeAt(index);
+                          });
                         },
+                        0,
                       ),
                     ],
                   ),
@@ -126,91 +180,94 @@ class _MainAppState extends State<MainApp> {
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  ...List.generate(
+                  ...buildLettres(
                     9,
                     (index) {
-                      return GestureDetector(
-                        onTap: () {
-                          if (usedLettres.contains(index)) {
-                            return;
-                          }
-                          setState(() {
-                            boiteLettres.add(lettres[index]);
-                            usedLettres.add(index);
-                          });
-                        },
-                        child: MouseRegion(
-                          cursor: usedLettres.contains(index)
-                              ? SystemMouseCursors.forbidden
-                              : SystemMouseCursors.click,
-                          child: Card(
-                            color: usedLettres.contains(index)
-                                ? Colors.grey[350]
-                                : Colors.white,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            child: SizedBox(
-                              width: 50,
-                              height: 50,
-                              child: Center(
-                                child: Text(
-                                  lettres[index],
-                                  style: TextStyle(
-                                    fontSize: 20,
-                                    color: Colors.cyan[900],
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
-                        ),
-                      );
+                      if (usedLettres.contains(index)) {
+                        return;
+                      }
+                      setState(() {
+                        boiteLettres.add(lettres[index]);
+                        usedLettres.add(index);
+                      });
                     },
+                    1,
                   ),
                 ],
               ),
               SizedBox(height: 30),
-              ElevatedButton(
-                onPressed: () async {
-                 final result =  await existance_mot(boiteLettres.join());
-                 if (result) {
-                   setState(() {
-                     mottrouves.add(boiteLettres.join());
-                     boiteLettres.clear();
-                     usedLettres.clear();
-                   });
-                 }
-                },
-                style: ButtonStyle(
-                  backgroundColor: WidgetStateProperty.all(
-                    Color.fromRGBO(176, 20, 85, 1),
-                  ),
-                  shape: WidgetStateProperty.all<RoundedRectangleBorder>(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                spacing: 20,
+                children: [
+                  IconButton(
+                      icon: Icon(Icons.close_rounded, size: 40),
+                      onPressed: () {
+                        setState(() {
+                          boiteLettres.clear();
+                          usedLettres.clear();
+                        });
+                      }),
+                  ElevatedButton(
+                    onPressed: () async {
+                      if (boiteLettres.isEmpty || boiteLettres.length < 2 || motTrouves.contains(boiteLettres.join())) {
+                        return;
+                      }
+                      final result = await existance_mot(boiteLettres.join());
+                      if (result == true) {
+                        setState(() {
+                          motTrouves.add(boiteLettres.join());
+                          boiteLettres.clear();
+                          usedLettres.clear();
+                        });
+                      }
+                    },
+                    style: ButtonStyle(
+                      backgroundColor: WidgetStateProperty.all(
+                        Color.fromRGBO(176, 20, 85, 1),
+                      ),
+                      shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      padding: WidgetStateProperty.all<EdgeInsets>(
+                        EdgeInsets.symmetric(
+                          horizontal: 10,
+                          vertical: 20,
+                        ),
+                      ),
+                    ),
+                    child: Text(
+                      "VALIDER",
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
-                  padding: WidgetStateProperty.all<EdgeInsets>(
-                    EdgeInsets.symmetric(
-                      horizontal: 10,
-                      vertical: 20,
-                    ),
-                  ),
-                ),
-                child: Text(
-                  "VALIDER",
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
+                  // IconButton(
+                  //     icon: Icon(Icons.loop_rounded, size: 40),
+                  //     onPressed: () {
+                  //       setState(() {
+                  //         boiteLettres.clear();
+                  //         usedLettres.clear();
+                  //         lettres.clear();
+                  //       });
+                  //     }),
+                ],
               ),
             ],
           ),
         ),
+      ),
+      theme: ThemeData(
+        splashColor: Colors.transparent,
+        highlightColor: Colors.transparent,
+        splashFactory: NoSplash.splashFactory,
+        hoverColor: Colors.transparent,
+        focusColor: Colors.transparent,
       ),
     );
   }
